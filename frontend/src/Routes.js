0a5ada93ecad13003d414ed3222e1463ebad7f23
Anthony Ramos <ramosanthony@gmail.com>
2023-07-13 11:29:37 -0400

    Add AppNav Component, Navbar, and Fixed Bugs

--- frontend/src/Routes.js
+++ frontend/src/Routes.js
@@ -1,34 +1,33 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
-import Companies from './Companies';
-import Company from './Company';
-import Home from './Home';
-import Jobs from './Jobs';
-import Login from './Login';
-import Profile from './Profile';
-import PrivateRoute from'./PrivateRoute';
+import Companies from "./Companies";
+import Company from "./Company";
+import Home from "./Home";
+import Jobs from "./Jobs";
+import Login from "./Login";
+import Profile from "./Profile";
 
-export default function Routes({ setToken }) {
+export default function Routes() {
   return (
     <Switch>
       <Route exact path="/">
         <Home />
       </Route>
-      <Route exact path="/login">
-        <Login setToken={setToken} />
-      </Route>
-      <PrivateRoute exact path="/companies">
+      <Route exact path="/companies">
         <Companies />
-      </PrivateRoute>
-      <PrivateRoute exact path="/companies/:handle">
+      </Route>
+      <Route exact path="/companies/:id">
         <Company />
-      </PrivateRoute>
-      <PrivateRoute exact path="/jobs">
+      </Route>
+      <Route exact path="/jobs">
         <Jobs />
-      </PrivateRoute>
-      <PrivateRoute exact path="/profile">
+      </Route>
+      <Route exact path="/login">
+        <Login />
+      </Route>
+      <Route exact path="/profile">
         <Profile />
-      </PrivateRoute>
+      </Route>
     </Switch>
   );
 }
